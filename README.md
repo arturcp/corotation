146aa886a81c2b81ae189ea683839d5e06e0860c
Artur <artur.prado@gmail.com>
2019-07-01 08:55:26 -0300

    Remove django

--- README.md
+++
@@ -1,9 +0,0 @@
-1. To enter in the environment:
-```
-$ pipenv shell
-```
-
-2. To run the server:
-```
-$ python manage.py runserver
-```
--- crawler/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Spider
-
-def crawlerView(request):
-    spider = Spider()
-    return render(request, "crawler.html", { 'clusters': spider.clusters })
